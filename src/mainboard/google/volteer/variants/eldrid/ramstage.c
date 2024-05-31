215479b13627c81ebacc76f14cec93ab569ce82c
ninelore <9l@9lo.re>
2024-06-01 00:49:59 +0200

    unfuck eldrid backup

--- src/mainboard/google/volteer/variants/eldrid/ramstage.c
+++ src/mainboard/google/volteer/variants/eldrid/ramstage.c
@@ -3,6 +3,7 @@
 #include <delay.h>
 #include <gpio.h>
 #include <baseboard/variants.h>
+#include <soc/ramstage.h>
 
 void variant_ramstage_init(void)
 {
@@ -16,4 +17,12 @@ void variant_ramstage_init(void)
 	gpio_output(GPP_A21, 1);
 	mdelay(1);
 	gpio_output(GPP_C23, 1);
+
+}
+
+// Unfucking ninelore's eldrid
+void mainboard_silicon_init_params(FSP_S_CONFIG *params) {
+	params->PchUsbOverCurrentEnable = 0;
+	params->PchPwrOptEnable = 0;
+
 }
